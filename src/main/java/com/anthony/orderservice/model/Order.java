326a2d6de61036efa48855ba5fa9b63cb132e3ba
anthony angatia <anthony.angatia@cellulant.io>
2024-03-11 10:06:05 +0300

    Few edits

--- src/main/java/com/anthony/orderservice/model/Order.java
+++ src/main/java/com/anthony/orderservice/model/Order.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Table(name = "orders")
 public class Order {
     @Id
--- src/main/java/com/anthony/orderservice/model/OrderItem.java
+++ src/main/java/com/anthony/orderservice/model/OrderItem.java
@@ -15,12 +15,13 @@ import java.math.BigDecimal;
 public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-            @Column(name = "item_id")
+    @Column(name = "item_id")
     Long orderItemID;
 
     @ManyToOne
     @JoinColumn(name = "order_id")
     @JsonBackReference
+    @JsonIgnore
     Order order;
 
     String name;
